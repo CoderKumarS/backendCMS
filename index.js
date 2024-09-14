213911b1511a179769b53bec8f0d3205a1bf0b0d
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2024-09-14 18:30:55 +0530

    Adding server to the project (Socket.io and express)

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const { Server } = require("socket.io")
+
+const cors = require("cors")
+const { log } = require('console')
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on('connection', (socket) => {
+    const id = socket.id;
+    console.log(`A user connected: ${id}`);
+    socket.on('send_message', (data) => {
+        socket.broadcast.emit('reci_message', data);
+    })
+    socket.on('disconnect', () => {
+        console.log(`A user disconnected: ${socket.id}`)
+    })
+})
+const port = 3000
+
+server.listen(port, () => console.log(`App listening on port ${port}!`))
